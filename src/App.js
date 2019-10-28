a9cad8737697c9a075e8b6d2cf8df148e722aea5
Juliano <julianotrabalho2016@gmail.com>
2019-10-28 00:01:57 -0300

    UPDATE - login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 import "./App.css";
 import Router from "./routes";
+import Logo from "./assets/logo.svg";
 
-const App = () => <Router />;
+const App = () => (
+  <div className="container">
+    <img src={Logo} alt="Logo" />
+    <div className="content">
+      <Router />
+    </div>
+  </div>
+);
 
 export default App;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
 import api from "../../services/api";
-import "./styles.css";
+//import "./styles.css";
 
-const Login = () => <h1>Login</h1>;
+const Login = ({ history }) => {
+  const [email, setEmail] = useState("");
 
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const response = await api.post("/sessions", {
+      email: email
+    });
+    const { _id } = response.data;
+    localStorage.setItem("@id", _id);
+    history.push("/dashboard");
+  };
+
+  const handleChange = e => {
+    setEmail(e.target.value);
+  };
+
+  return (
+    <Fragment>
+      <p>
+        Ofereça <strong>spots </strong>para programadores e encontre
+        <strong> talentos</strong> para sua empresa
+      </p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">E-MAIL *</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          placeholder="Seu melhor e-mail"
+          onChange={handleChange}
+        />
+        <button type="submit">Entar</button>
+      </form>
+    </Fragment>
+  );
+};
 export default Login;
