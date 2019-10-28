64ec3c246c7fe96cb67b81a434f0c1d87e658fcc
Juliano <julianotrabalho2016@gmail.com>
2019-10-28 12:59:46 -0300

    UPDATE - Dashboard page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useEffect, useState, Fragment } from "react";
+import { Link } from "react-router-dom";
 import api from "../../services/api";
 import "./styles.css";
 
-const Dashboard = () => <h1>Dashboard</h1>;
+const Dashboard = () => {
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    const loadSposts = async () => {
+      const user_id = localStorage.getItem("@id");
+      const response = await api.get("/dashboard", {
+        headers: { user_id }
+      });
+      setSpots(response.data);
+    };
+    loadSposts();
+  }, []);
+  return (
+    <Fragment>
+      <ul className="spot-list">
+        {spots.map(spot => (
+          <li key={spot._id}>
+            <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
+            <strong>{spot.company}</strong>
+            <span>{spot.price ? `R$ ${spot.price} por dia` : "GRATUITO"}</span>
+          </li>
+        ))}
+      </ul>
+      <Link to="/new">
+        <button>Cadastrar novo spot</button>
+      </Link>
+    </Fragment>
+  );
+};
 
 export default Dashboard;
